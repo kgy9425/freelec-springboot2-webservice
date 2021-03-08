986914e3e26bfe0dabdb11557b5aa73d30efb9a7
kgy9425 <kim963^^>
2021-03-08 23:29:23 +0900

    implement CRUD function(except delete)

--- src/main/java/com/gayoung/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/gayoung/book/springboot/domain/posts/Posts.java
@@ -1,4 +1,41 @@
 package com.gayoung.book.springboot.domain.posts;
 
-public class Posts {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter //롬복
+@NoArgsConstructor //롬복
+@Entity //JPA
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length =  500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
